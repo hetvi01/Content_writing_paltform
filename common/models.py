9d938760710148941c0688c59730c434f4df5525
Hetvi <hetvip.inexture@gmail.com>
2023-05-06 16:59:48 +0530

    feat: Add common app - Add Common model to reuse it in other models

--- common/models.py
+++ common/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils.timezone import now
 
@@ -6,9 +8,11 @@ from contentWritingPlatform.settings import AUTH_USER_MODEL
 
 # Create your models here.
 class Common(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = models.DateTimeField(default=now)
     created_by = models.ForeignKey(AUTH_USER_MODEL,
                                    related_name='%(class)s_createdby', on_delete=models.SET_NULL, null=True, blank=True)
+
 
     class Meta:
         abstract = True
