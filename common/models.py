7bdb94b9abbcfd9e376e3d2e533cb91ab8840988
Hetvi <hetvip.inexture@gmail.com>
2023-05-06 17:03:57 +0530

    feat: Add common app - Add Common model to reuse it in other models

--- common/models.py
+++ common/models.py
@@ -8,11 +8,16 @@ from contentWritingPlatform.settings import AUTH_USER_MODEL
 
 # Create your models here.
 class Common(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     created_at = models.DateTimeField(default=now)
     created_by = models.ForeignKey(AUTH_USER_MODEL,
                                    related_name='%(class)s_createdby', on_delete=models.SET_NULL, null=True, blank=True)
 
+    class Meta:
+        abstract = True
+
+
+class UUID(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
 
     class Meta:
         abstract = True
