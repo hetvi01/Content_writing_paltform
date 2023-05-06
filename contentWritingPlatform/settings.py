7dc52247940163f35cf8438864e3c2a3e66b6b08
Hetvi <hetvip.inexture@gmail.com>
2023-05-06 17:11:26 +0530

    feat: Add common app - Add Common model to reuse it in other models

--- contentWritingPlatform/settings.py
+++ contentWritingPlatform/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lrp408p(%%dzkrww824#-*^!7w)%wufqb%jhhfq1=x4-loffo+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
